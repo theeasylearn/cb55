ee2c224664666e8a2a6dab2a7d1d6d3f8cb7ac5c
theeasylearn <theeasylearn@gmail.com>
2025-09-24 09:33:49 +0530

    array of structure

---
+++ 45_struct_3.c
@@ -0,0 +1,47 @@
+// passing array as argument in user defined function
+#include <stdio.h>
+// create structure
+struct result
+{
+    char name[32]; // array within structure
+    int maths, science, english, total;
+    float percentage;
+};
+//pass by reference
+void input(struct result batch[])
+{
+
+    int index = 0;
+    while (index < 5)
+    {
+        fflush(stdin);
+        printf("\n Enter %d your name", index + 1);
+        gets(batch[index].name);
+        printf("Enter maths marks");
+        scanf("%d", &batch[index].maths);
+        printf("Enter science marks");
+        scanf("%d", &batch[index].science);
+        printf("Enter english marks");
+        scanf("%d", &batch[index].english);
+
+        batch[index].total = batch[index].maths + batch[index].science + batch[index].english;
+        batch[index].percentage = batch[index].total / 3.0;
+        index++;
+    }
+}
+void display(struct result batch[])
+{
+    int index;
+    for (index = 0; index < 5; index++)
+    {
+        printf("\n %d Student name = %s", index + 1, batch[index].name);
+        printf("\nmaths = %d science = %d english = %d total = %d percentage = %.2f", batch[index].maths, batch[index].science, batch[index].english, batch[index].total, batch[index].percentage);
+    }
+}
+void main()
+{
+    // create array of structure type variable
+    struct result batch[5];
+    input(batch);
+    display(batch);
+}
